1e2db75d7efcf62cb08aa5b84665921dec6c1640
蔡磊 <cl@B-38YVMD6R-0314.local>
2022-02-15 15:19:04 +0800

    提交1

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tv_hello);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
