404dcd406ed9bb04080f7dbe824f92ede5cafe73
蔡磊 <cl@B-38YVMD6R-0314.local>
2022-02-15 16:50:30 +0800

    add print this is develop

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = (TextView) findViewById(R.id.tv_hello);
-
+        System.out.println("this is develop");
     }
 }
